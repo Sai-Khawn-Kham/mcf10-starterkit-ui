5594f8c19030cdd6a9994e823b426a93d5167354
Sai-Khawn-Kham <khawnkham222@gmail.com>
2026-01-10 23:01:56 +0630

    feat : create route

---
+++ app/setting/appearance/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Appearance Page</div>;
+};
+
+export default Page;
---
+++ app/setting/password/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Password Page</div>;
+};
+
+export default Page;

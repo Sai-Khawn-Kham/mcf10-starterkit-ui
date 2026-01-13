0e6556fb3d75d9d32c2997264104225eb848f893
Sai-Khawn-Kham <khawnkham222@gmail.com>
2026-01-13 11:25:57 +0630

    feat : login and register for auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import logo from "@/assets/logo-with-white-border.svg";
+import Image from "next/image";
 
 export default function AuthLayout({
   children,
@@ -5,6 +8,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div id="auth-layout">{children}</div>
+    <div id="auth-layout" className="flex min-h-svh flex-col items-center justify-center gap-9 p-9">
+      <div className="flex w-full max-w-md flex-col gap-6">
+        <Link href="/" className="flex items-center gap-3 self-center font-medium">
+          <div className="flex size-12 items-center justify-center">
+            <Image src={logo} width={44} height={44} alt="Logo" />
+          </div>
+        </Link>
+        {children}
+      </div>
+    </div>
   );
 }

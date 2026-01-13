a50285ed4ae00bf9d5b51a859ed212735b05361a
Sai-Khawn-Kham <khawnkham222@gmail.com>
2026-01-14 03:09:08 +0630

    fix : fix issues for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
 import "../styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const notoSans = Noto_Sans({variable:'--font-sans'});
 
@@ -25,11 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={notoSans.variable}>
+    <html lang="en" className={notoSans.variable} suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- features/settings/components/AppearanceSection.tsx
+++ features/settings/components/AppearanceSection.tsx
@@ -1,7 +1,36 @@
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { ComputerIcon, MonitorIcon, MoonIcon, SunIcon } from "lucide-react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { MonitorIcon, MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function AppearanceSection() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Prevent hydration mismatch by waiting until mounted
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const themeOptions = [
+    {
+      title: "light",
+      icon: SunIcon,
+    },
+    {
+      title: "dark",
+      icon: MoonIcon,
+    },
+    {
+      title: "system",
+      icon: MonitorIcon,
+    },
+  ];
+
+  if (!mounted) return null;
+
   return (
     <section>
       <h1 className=" font-semibold">Appearance settings</h1>
@@ -9,21 +38,24 @@ export default function AppearanceSection() {
         Update your account's appearance settings
       </p>
 
-      <ToggleGroup
-        type="single"
-        defaultValue={"c"}
-        className=" mt-5 flex justify-between items-center gap-1.5 bg-gray-300 dark:bg-gray-700 p-1 "
-      >
-        <ToggleGroupItem value="a" className="text-md">
-          <SunIcon /> Light
-        </ToggleGroupItem>
-        <ToggleGroupItem value="b" className="text-md">
-          <MoonIcon /> Dark
-        </ToggleGroupItem>
-        <ToggleGroupItem value="c" className="text-md">
-          <MonitorIcon /> System
-        </ToggleGroupItem>
-      </ToggleGroup>
+      <div className="mt-5 inline-flex justify-between items-center gap-2 bg-gray-300 dark:bg-gray-700 p-1">
+        {themeOptions.map((item) => (
+          <Button
+            key={item.title}
+            onClick={() => setTheme(item.title)}
+            className={cn(
+              "text-md p-5",
+              theme === item.title
+                ? "bg-gray-200 text-black dark:bg-gray-600 dark:text-white"
+                : "bg-gray-300 text-gray-500 dark:bg-gray-700"
+            )}
+          >
+            <div className="flex items-center gap-2">
+              <item.icon /> <span className=" capitalize">{item.title}</span>
+            </div>
+          </Button>
+        ))}
+      </div>
     </section>
   );
 }
